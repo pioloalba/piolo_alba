d621f7d299f88fe776e979bd2a9f716bc9960365
unknown <pioloalba12345@gmail.com>
2025-09-19 12:44:29 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'lava_lust_db_test',
+    'username'	=> 'sql12799046',
+    'password'	=> 'ib1PYTdL4p',
+    'database'	=> 'sql12799046',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
